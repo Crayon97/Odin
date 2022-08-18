d01e1aae579a3c42d17d9a8c719ce8006980febf
Crayon97 <89330017+Crayon97@users.noreply.github.com>
2022-08-18 18:09:30 +0530

    Add files via upload

---
+++ rockPaper.js
@@ -0,0 +1,18 @@
+console.log("Rock Paper test")
+
+// 0 for Rock
+// 1 for Paper
+// 2 for Scissor 
+
+function getComputerChoice(){
+    let rand = Math.floor(Math.random()*3)
+    
+    return rand
+}
+
+function 
+
+let retVal = getComputerChoice();
+console.log(retVal)
+
+
