550ea5d863d7b23980d3ae3687d9b73a8a70b91b
Roshan S <roshansalian1997@gmail.com>
2022-08-18 23:01:03 +0530

    basic

--- rockPaper.js
+++ rockPaper.js
@@ -4,13 +4,24 @@ console.log("Rock Paper test")
 // 1 for Paper
 // 2 for Scissor 
 
-function getComputerChoice(){
-    let rand = Math.floor(Math.random()*3)
-    
+function getComputerChoice() {
+    let rand = Math.floor(Math.random() * 3)
+
     return rand
 }
 
-function 
+function getUserChoice() {
+    let userChoice = input("Enter Rock, paper or scissor")
+    let correctedUserChoice = userChoice.toLowerCase()
+
+    if (correctedUserChoice != 'rock' || correctedUserChoice != 'paper' || correctedUserChoice != 'scissor') {
+        alert("Invalid choice entered")
+        return
+    }
+
+
+
+}
 
 let retVal = getComputerChoice();
 console.log(retVal)
