a681300c45e74296a5a474fa0a577db6adc5423f
Roshan S <roshansalian1997@gmail.com>
2022-08-19 23:31:58 +0530

    Grid appears

---
+++ Sketch/javas.js
@@ -0,0 +1,26 @@
+const container = document.querySelector('#containerId')
+
+// const div = document.createElement('div')
+// div.classList.add('divClass')
+// div.style.width = "90px"
+// div.style.height = "90px"
+// div.style.borderBlockColor = "black"
+// div.style.backgroundColor = "black"
+
+// var content = 
+
+for (let i = 0; i < 16; i++) {
+    const div = document.createElement('div')
+    div.classList.add('divClass' + i)
+    div.style.width = "90px"
+    div.style.height = "90px"
+    div.style.borderBlockColor = "black"
+    div.style.backgroundColor = "blue"
+    div.style.display = "inline-block"
+    div.style.margin = "1px"
+
+    container.appendChild(div)
+}
+
+container.appendChild(div)
+
